05e3ae364998b0f5e08944258863772b39a2341a
Soumyajit Kundu <52736752+soumyajitkundu@users.noreply.github.com>
2019-08-12 13:46:55 +0530

    Update fibo.c

--- fibo.c
+++ fibo.c
@@ -11,7 +11,7 @@ int main()
 	int n,i,j=0;
 	printf("Enter the term upto which you want to print the Fibonacci Series");
 	scanf("%d",&n);
-	for(i=0;i<=n;i++)
+	for(i=0;i<n;i++)
 	{
 		printf("%d\t",fibo(j));
 		j++;
